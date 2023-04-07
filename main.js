9e1c34a5c31bdcc86126d0e2b8de0df4b3ee2a90
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-08 00:37:54 +0200

    Map para transformar elemento a una lista + join para agregar elementos a mi lista

--- main.js
+++ main.js
@@ -11,9 +11,22 @@ window.onload = () => {
  todo.value = ''; // reemplazamos su valor por string vacio
  todos.push(todoText); //agrego elementos a un array
  const todoList = document.getElementById('todo-list');
- todoList.innerHTML = '';
- for(let i = 0; i < todos.length; i++) {
-    todoList.innerHTML += '<li>' + todos[i] + '</li>';//concatenando
-  }
- }
+ //todoList.innerHTML = '';
+ //for(let i = 0; i < todos.length; i++) {//para que no se duplique
+   // todoList.innerHTML += '<li>' + todos[i] + '</li>';//concatenando
+  //}
+  //AGREGANDO EL MAP PARA TENER EJEMPLO
+//  const todosTemplate = todos.map(t =>{
+//    return 'li' + t + '/li';
+
+ // }) // dentro de () le coloco el ellemnto que estoy iterando
+ // console.log(todosTemplate);
+ const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+ todoList.innerHTML = todosTemplate.join('') //JOIN SE USA PARA AGREGAR MAS ELEMNTOS A MI LI
 }
+}
+// APRENDIENDO .MAP
+//Map agrega al elemento del arreglo lo que nosotros devolvemos
+//Transforma el elemento dentro de lo que nosotros devolvemos
+
+//.JOIN lo que hace es agregar mas elementos a mi lista de ul del html
