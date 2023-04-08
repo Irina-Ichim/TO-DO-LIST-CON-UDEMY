264c17debe699120d6e33a5133c19a03a7222dde
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-04-09 01:01:23 +0200

    aprendiendo .addEventListner

--- main.js
+++ main.js
@@ -3,28 +3,28 @@ const todos = []; //para guardar en la memoria interna todos los 'todo'
 
 
 window.onload = () => {
- const form = document.getElementById('todo-form'); //tomamos nuestro formulario
- form.onsubmit = (e) => {  // reemplazamos con onsubmit
+const form = document.getElementById('todo-form'); 
+form.onsubmit = (e) => {  // reemplazamos con onsubmit
  e.preventDefault(); //para que no se refrescue nuestra web
  const todo = document.getElementById('todo'); //
- const todoText = todo.value ;
- todo.value = ''; // reemplazamos su valor por string vacio
- todos.push(todoText); //agrego elementos a un array
- const todoList = document.getElementById('todo-list');
+ //const todoText = todo.value ;
+ //todo.value = ''; // reemplazamos su valor por string vacio
+ //todos.push(todoText); //agrego elementos a un array
+ //const todoList = document.getElementById('todo-list');
  //todoList.innerHTML = '';
- //for(let i = 0; i < todos.length; i++) {//para que no se duplique
-   // todoList.innerHTML += '<li>' + todos[i] + '</li>';//concatenando
-  //}
+ for(let i = 0; i < todos.length; i++) {//para que no se duplique
+    todoList.innerHTML += '<li>' + todos[i] + '</li>';//concatenando
+  }
   //AGREGANDO EL MAP PARA TENER EJEMPLO
-//  const todosTemplate = todos.map(t =>{
-//    return 'li' + t + '/li';
+  //const todosTemplate = todos.map(t =>{
+    //return 'li' + t + '/li';
 
- // }) // dentro de () le coloco el ellemnto que estoy iterando
+ //}) // dentro de () le coloco el ellemnto que estoy iterando
  // console.log(todosTemplate);
- const todosTemplate = todos.map(t => '<li>' + t + '</li>');
- todoList.innerHTML = todosTemplate.join('') //JOIN SE USA PARA AGREGAR MAS ELEMNTOS A MI LI
-}
-}
+ //const todosTemplate = todos.map(t => '<li>' + t + '</li>');
+ //todoList.innerHTML = todosTemplate.join('') //JOIN SE USA PARA AGREGAR MAS ELEMNTOS A MI LI
+//}
+//}
 // APRENDIENDO .MAP
 //Map agrega al elemento del arreglo lo que nosotros devolvemos
 //Transforma el elemento dentro de lo que nosotros devolvemos
@@ -38,7 +38,25 @@ window.onload = () => {
 //Querry SelectorAll_instruccion para ir a buscar elementos dentro de mi listado y agregarles una funcion que se ejecuta cuando le hago click a uno de estos elementos
 //Te permite buscar por clase, por id, por etiqueta que tengas en html poniendo(#elemento que quieres agregar)
 
-document.querySelectorAll('#todo-list');
-const elementos = document.querySelectorAll('#todo-list li') //llamo a los elementos li
+//document.querySelectorAll('#todo-list');
+//const elementos = document.querySelectorAll('#todo-list li') //llamo a los elementos li
 //Si quieres llamar a los elementos de un array tambien puedes utilizar Array.from(elemetos)
-elementos.forEach( x=> console.log(x))
+//elementos.forEach( x=> console.log(x))
+
+//Para agregar un evento usa add.EventListener(type, listener), siendo type el tipo de evento como click y listener lo que queremos que pasé;
+
+const todoText = todo.value;
+todo.value = '';
+todos.push(todoText);
+const todoList = document.getElementById('todo-list');
+const todosTemplate = todos.map(t => '<li> + t +</li>');
+todoList.innerHTML = todosTemplate.join('');
+const elementos = document.querySelectorAll('#todo-list')
+elementos.forEach((elemento, i) => {
+  elemento.addEventListener('click', () =>
+  console.log(elemento, i));
+  
+})
+}
+}
+
